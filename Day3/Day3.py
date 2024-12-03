5680746de42286ddfe05bfb7fd230f637bf129fe
jonklaric <jon.klaric@gmail.com>
2024-12-03 20:57:12 +1000

    added day3 part 2 solution.

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -32,15 +32,54 @@ def day3_part1(input_file_location):
         #print(search_string.split(",", 1))
         numbers = search_string.split(",", 1)
         if all([chars.isnumeric() for chars in numbers]):
-            print(numbers[0], "x", numbers[1])
             running_sum += np.prod([int(chars) for chars in numbers])
         else:
             continue        
     return running_sum
 
-test_input_file = os.path.dirname(__file__) + "\\test_input.txt"
-print(read_file(test_input_file))
-print(day3_part1(test_input_file))
-my_input_file = os.path.dirname(__file__) + "\\my_input.txt"
-print(day3_part1(my_input_file))
 
+def day3_part2(input_file_location):
+    running_sum = 0
+    text = read_file(input_file_location)
+    do_locations = find_all_occurences(text, "do()")
+    print("do_locations", do_locations)
+    do_not_locations = find_all_occurences(text, "don't()")
+    print("do_not_locations", do_not_locations)
+    do_or_dont = True
+    do_dont_list = []
+    for i in range(len(text)):
+        if len(do_locations) > 0 and i==do_locations[0]:
+            do_or_dont = True
+            do_locations.pop(0)
+        elif len(do_not_locations) > 0 and i == do_not_locations[0]:
+            do_or_dont = False
+            do_not_locations.pop(0)
+        do_dont_list.append(do_or_dont)
+    text = "".join([text[i] if do_dont_list[i] else "*" for i in range(len(text)) ])
+    #print(text)
+    locations = find_all_occurences(text, "mul(")
+    #print("mul locations", locations)
+    for loc in locations:
+        search_string = text[loc+4:loc+13]
+        if ")" not in search_string or "," not in search_string:
+            continue
+        search_string = search_string[:search_string.find(")")]
+        print(search_string.split(",", 1))
+        numbers = search_string.split(",", 1)
+        if all([chars.isnumeric() for chars in numbers]):
+            running_sum += np.prod([int(chars) for chars in numbers])
+        else:
+            continue        
+    return running_sum
+
+
+
+# test_input_file = os.path.dirname(__file__) + "\\test_input.txt"
+# print(read_file(test_input_file))
+# print(day3_part1(test_input_file))
+my_input_file = os.path.dirname(__file__) + "\\my_input.txt"
+# print(day3_part1(my_input_file))
+test_input_file = os.path.dirname(__file__) + "\\test_input_part2.txt"
+print(day3_part2(test_input_file))
+
+print(day3_part2(my_input_file))
